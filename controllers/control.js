eaf58a3b0124c13dfc74014090818c7c230a37c7
Bhaskar Singh <bhaskarsingh161197@gmail.com>
2018-11-19 22:34:33 +0530

    after search can go to the result page which has reviews and changed some nav icons

--- controllers/control.js
+++ controllers/control.js
@@ -42,19 +42,72 @@ module.exports = (app, con, urlencodedParser, session) => {
         }
         
     })
+
+    //review Submit
+
+    app.post('/reviewSubmit', urlencodedParser, (req,res) => {
+        console.log("POST Request @ -> " + req.url)
+        let body = req.body
+        let reviewe = body.id
+        let reviewer = req.session.user
+        let review = body.review
+        let rating = body.rating
+        let qs = "INSERT INTO review VALUES(null,?,?,?,?)"
+        let data = [reviewe, reviewer, review, rating]
+        con.query(qs, data, (err, results) => {
+            if (err) console.error(err)
+            console.log("successfull insertion of the review")
+
+        })
+
+        setTimeout(() => {
+            res.redirect(req.headers.referer)
+        }, 1)
+    })
+
+
+
     //provider Profile
     app.get('/providerProfile', (req, res) => {
         console.log("Get Request @ -> " + req.url)
         console.log('this is referer -> ' + req.headers.referer)
         let id = req.query.id
+        let row
+        let row2
         let qs = "SELECT * FROM user WHERE id=" + id
         con.query(qs, (err, results) => {
             if(err) throw err;
-            console.log(results);
+            // console.log(results);
+            row = results
         })
+
         setTimeout(() => {
-            res.redirect(req.headers.referer)
-        }, 50)
+            qs = "SELECT * FROM review r, user u where u.id=r.reviewer AND reviewe=" + id;
+            con.query(qs, (err, result) => {
+                if (err) console.error(err)
+                row2 = result
+            })
+        }, 100)
+
+        setTimeout(() => {
+
+            console.log("This is review rows", row2)
+
+            if(req.session.user){
+                if(row.length == 0){
+                    res.render('providerProfile', {user: true, provider: null, reviews: row2, id: id})
+                }else{
+                    res.render('providerProfile', {user: true, provider: row[0], reviews: row2, id: id})
+                }
+            }
+            else{
+                if(row.length == 0){
+                    res.render('providerProfile',{user: false, provider: null, reviews: row2, id: id})
+                }else{
+                    res.render('providerProfile', {user: false, provider: row[0], reviews: row2, id: id})
+                }
+            }
+        }, 200)
     })
 
     //search
@@ -114,7 +167,7 @@ module.exports = (app, con, urlencodedParser, session) => {
                         if(clientUser === req.body.email){
                             console.log("Email is same")
                             if(clientPassword === req.body.password){
-                                req.session.user = req.body.email
+                                req.session.user = result[0].id
                                 console.log("Password is same " + req.session.user)
                                 // res.redirect('/protected')
                             }
