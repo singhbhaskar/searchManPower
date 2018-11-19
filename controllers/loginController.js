13f0080e6b4a491d1494ec04934854f6a54a44d4
Bhaskar Singh <bhaskarsingh161197@gmail.com>
2018-11-19 23:55:21 +0530

    rearrange post signup to loginContoller.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,7 @@
 // invalid Login
 // GET signup
 // GET login
-
+//POST signUp
 
 module.exports = (app, con, urlencodedParser) => {
     //logout
@@ -101,5 +101,30 @@ module.exports = (app, con, urlencodedParser) => {
         console.log("Get Request @ -> " + req.url);
         console.log('this is referer' + req.headers.referer)
         res.render('login')
+    })
+
+     //POST signUp new user
+     app.post('/signup', urlencodedParser,  (req,res) => {
+        console.log("Post Request @ -> " + req.url);
+        console.log(req.body)
+        let body = req.body
+        let first = body.first_name
+        let last = body.last_name
+        let email = body.email
+        let pass = body.password
+        let dob = body.dob
+        let gender = body.gender
+        let status = body.status
+
+        let query = "INSERT INTO user VALUES (null,?, ?, ?, ?, ?, ?, ?);"
+        let data = [first, last, email, pass, dob, gender, status];
+        con.query(query, data, (err, result) => {
+            if (err) throw err
+            console.log("insert success fully the records", result, "end of insert query")
+        })
+
+        setTimeout(() => {
+            res.redirect('/')
+        }, 100)
     })
 };
--- controllers/staticController.js
+++ controllers/staticController.js
@@ -7,29 +7,4 @@ module.exports = (app, con, urlencodedParser) => {
         res.redirect(req.headers.referer)
     })
 
-
-    //signUp new user
-    app.post('/signup', urlencodedParser,  (req,res) => {
-        console.log("Post Request @ -> " + req.url);
-        console.log(req.body)
-        let body = req.body
-        let first = body.first_name
-        let last = body.last_name
-        let email = body.email
-        let pass = body.password
-        let dob = body.dob
-        let gender = body.gender
-        let status = body.status
-
-        let query = "INSERT INTO user VALUES (null,?, ?, ?, ?, ?, ?, ?);"
-        let data = [first, last, email, pass, dob, gender, status];
-        con.query(query, data, (err, result) => {
-            if (err) throw err
-            console.log("insert success fully the records", result, "end of insert query")
-        })
-
-        setTimeout(() => {
-            res.redirect('/')
-        }, 100)
-    })
 };
